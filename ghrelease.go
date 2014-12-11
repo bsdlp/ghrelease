63de136913c751e2448e1394d4fe600f27c75ee7
Jon Chen <bsd@voltaire.sh>
2014-12-11 00:04:17 -0500

    Revert "FindGox() to return *string"
    This reverts commit 67c5415aee7e851e822e92b29fa034acb3aae439.

--- ghrelease.go
+++ ghrelease.go
@@ -48,17 +48,17 @@ func init() {
 	CurrentUser, _ := user.Current()
 	DefaultConfigPath := path.Join(CurrentUser.HomeDir, ".config/ghrelease/config.json")
 	ConfigPath = flag.String([]string{"c", "-config"}, DefaultConfigPath, "Set ghrelease config file")
-	GoxPath = flag.String([]string{"g", "-gox"}, *FindGox(), "Path to gox")
+	GoxPath = flag.String([]string{"g", "-gox"}, FindGox(), "Path to gox")
 
 	flag.Parse()
 }
 
-func FindGox() *string {
+func FindGox() string {
 	path, err = exec.LookPath("gox")
 	if err != nil {
 		log.Fatalln("gox is not in your $PATH")
 	}
-	return *path
+	return path
 }
 
 func Gox(BuildFlags *[]string) *string {
